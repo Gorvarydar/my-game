decab671b11fbaa2ecaad6ace7554f233519fc58
Gorvarydar <bothisav@gmail.com>
2021-07-01 23:17:54 +0300

    '1'

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: path.resolve(__dirname, 'src/index.js'),
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    devtool: 'eval-source-map',
+  },
+  watch: true,
+  watchOptions: {
+    ignored: /node_modules/,
+    poll: 1000,
+  },
+  optimization: {
+    minimize: false,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: [/node_modules/],
+      },
+    ],
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: path.resolve(__dirname, 'public/index.html'),
+    }),
+  ],
+  devServer: {
+    port: 3000,
+    overlay: true,
+    open: true,
+    hot: true,
+    historyApiFallback: true,
+  },
+
+};
